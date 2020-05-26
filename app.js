d854c2acd5592346d5c0dcd94d1bbbbf229fd1d8
Djccowcow <djccowcow@gmail.com>
2020-05-26 20:20:49 +0300

    Added password alphanumeric validation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const db = require("./database.js");
 const crypto = require("crypto")
 let username = 'asdas'
-let userpass = 'domatic'
+let userpass = 'domatic@#'
 
 /*let mykey = crypto.createCipher('aes-128-cbc', userpass);
 let passhash = mykey.update('abc', 'utf8', 'hex');
@@ -13,19 +13,24 @@ passhash += mykey.final('hex');*/
 
 //Check if the password contains only letters and numbers.
 function alphanumValidation(userpass){
-    let alphanum = /^[0-9a-zA-Z]+$/;
-    if(userpass.value.match(alphanum)){
+    let alphaNum = /^[0-9a-zA-Z]+$/;
+    if(userpass.match(alphaNum)){
         return true;
+        //Hash password
+        
+        //db.sqlinsert(username, passhash);
     }else{
         return false;
     }
 }
+
 if(alphanumValidation(userpass)){
     console.log("Returned: True");
 }else{
     console.log("Returned: False");
 }
-//db.sqlinsert(username, passhash);
+
+
 app.use("/public", express.static("public"));
 
 //Manage html pages
