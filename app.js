5adf1a21a95a39c51c34265568b33601ad424c5a
Djccowcow <djccowcow@gmail.com>
2020-05-26 13:07:25 +0300

    nodejs implementation

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use('/public', express.static('public'))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.get('/registration.html', function (req, res) {
+    res.sendFile(path.join(__dirname, '/registration.html'))
+})
+
+app.listen(3000)
